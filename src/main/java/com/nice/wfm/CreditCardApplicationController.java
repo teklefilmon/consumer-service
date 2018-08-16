3e4a904d315931883fd6469cda828f73b8b63e38
Filmon Tekle <teklefilmon@yahoo.com>
2018-08-16 15:30:31 -0500

    refactor provider-service base url

--- src/main/java/com/nice/wfm/CreditCardApplicationController.java
+++ src/main/java/com/nice/wfm/CreditCardApplicationController.java
@@ -4,11 +4,13 @@ import com.nice.wfm.dto.CreditCardApplicationResponse;
 import com.nice.wfm.dto.CreditCardApplicationRequest;
 import com.nice.wfm.dto.CreditScoreRequest;
 import com.nice.wfm.dto.CreditScoreResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 
@@ -22,16 +24,22 @@ import java.util.Objects;
 public class CreditCardApplicationController {
 
     private final RestTemplate restTemplate;
+    private final String providerServiceBaseUrl;
 
-    public CreditCardApplicationController(RestTemplate restTemplate){
+    public CreditCardApplicationController(RestTemplate restTemplate, @Value("${provider-service.baseurl}") String providerServiceBaseUrl){
         this.restTemplate = restTemplate;
+        this.providerServiceBaseUrl = providerServiceBaseUrl;
         Objects.requireNonNull(this.restTemplate);
+        Objects.requireNonNull(this.providerServiceBaseUrl);
     }
 
     @PostMapping(value = "/credit-card-applications", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public CreditCardApplicationResponse applyForCreditCard(final @RequestBody CreditCardApplicationRequest creditCardApplicationRequest){
         String ssn = creditCardApplicationRequest.getSsn();
-        CreditScoreResponse creditScoreResponse = restTemplate.postForObject("http://localhost:8080/credit-scores", new CreditScoreRequest(ssn), CreditScoreResponse.class);
+
+        String url = UriComponentsBuilder.fromHttpUrl(providerServiceBaseUrl)
+                .path("credit-scores").toUriString();
+        CreditScoreResponse creditScoreResponse = restTemplate.postForObject(url, new CreditScoreRequest(ssn), CreditScoreResponse.class);
 
         if(creditScoreResponse.isCreditScoreHigh() && CreditCardApplicationRequest.CardType.GOLD == creditCardApplicationRequest.getCardType()){
             return new CreditCardApplicationResponse(CreditCardApplicationResponse.Status.GRANTED);
