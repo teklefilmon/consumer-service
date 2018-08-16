4c08b720e66e08aa14f4805f45484caccf4d9de1
Filmon Tekle <teklefilmon@yahoo.com>
2018-08-16 15:19:54 -0500

    configure stub runner

--- src/test/java/com/nice/wfm/ConsumerServiceApplicationTests.java
+++ src/test/java/com/nice/wfm/ConsumerServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureStubRunner(ids = "com.nice.wfm:provider-service:+:stubs:8080", workOffline = true)
 public class ConsumerServiceApplicationTests {
 
     @Autowired
